23e4ca0f4e1b8cd5365d2477be13b0822faf9f74
Ion <ordonez.ian13@gmail.com>
2023-04-29 16:08:40 -0400

    deleted hello.py, changed model

--- model.py
+++ model.py
@@ -1,56 +1,70 @@
 import torch
-import random
+import torch.nn as nn
+import torch.optim as optim
 
-# Define the problem objective function
-def objective_function(schedule):
-    # Calculate the total time taken to complete all tasks in the schedule
-    total_time = 0
-    for task in schedule:
-        total_time += task.duration
-    return total_time
+import csv
+import datetime
 
-# Define the state space
-def generate_random_schedule(num_tasks):
-    schedule = []
-    for i in range(num_tasks):
-        task = {
-            'name': f'Task {i+1}',
-            'duration': random.randint(1, 8) # randomly generate task duration between 1 and 8 hours
-        }
-        schedule.append(task)
-    return schedule
+# Define users
+users = ['user' + str(i) for i in range(1, 101)]
 
-# Define the transition probability function
-def swap_tasks(schedule):
-    # randomly swap the order of two tasks in the schedule
-    i, j = random.sample(range(len(schedule)), 2)
-    schedule[i], schedule[j] = schedule[j], schedule[i]
-    return schedule
+# Define event types
+event_types = ['Meeting', 'Lunch', 'Presentation', 'Training', 'Appointment']
 
-# Define the acceptance probability function
-def acceptance_probability(delta, temperature):
-    return torch.exp(-delta / temperature)
+# Define start and end dates
+start_date = datetime.date(2023, 5, 1)
+end_date = datetime.date(2023, 5, 31)
 
-# Set the temperature parameter
-temperature = 1.0
+# Load data from CSV file
+events = []
+with open('calendar_events.csv', mode='r') as file:
+    reader = csv.reader(file)
+    next(reader) # Skip header row
+    for row in reader:
+        user, event_type, date, start_time, end_time = row
+        events.append([users.index(user), event_types.index(event_type), (datetime.datetime.strptime(date, '%Y-%m-%d').toordinal() - start_date.toordinal()), (datetime.datetime.strptime(start_time, '%H:%M:%S').hour), (datetime.datetime.strptime(end_time, '%H:%M:%S').hour)])
 
-# Initialize the current state
-current_schedule = generate_random_schedule(num_tasks)
+# Convert data to PyTorch tensors
+events = torch.tensor(events, dtype=torch.float32)
 
-# Run the MCMC algorithm
-for i in range(num_iterations):
-    # Propose a new state
-    new_schedule = swap_tasks(current_schedule)
-    
-    # Calculate the change in objective function value
-    delta = objective_function(new_schedule) - objective_function(current_schedule)
-    
-    # Calculate the acceptance probability
-    accept_prob = acceptance_probability(delta, temperature)
-    
-    # Decide whether to accept or reject the proposed state
-    if accept_prob > random.uniform(0, 1):
-        current_schedule = new_schedule
-    
-# Extract the optimal schedule
-optimal_schedule = current_schedule
+# Split data into training and validation sets
+train_data = events[:400]
+val_data = events[400:]
+
+# Define PyTorch model
+class EventDurationModel(nn.Module):
+    def __init__(self):
+        super(EventDurationModel, self).__init__()
+        self.fc1 = nn.Linear(4, 16)
+        self.fc2 = nn.Linear(16, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.Adam(EventDurationModel.parameters(), lr=0.01)
+
+# Train PyTorch model
+model = EventDurationModel()
+num_epochs = 100
+batch_size = 32
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i in range(0, len(train_data), batch_size):
+        batch = train_data[i:i+batch_size]
+        optimizer.zero_grad()
+        outputs = model(batch[:, 0:4])
+        loss = criterion(outputs, batch[:, 4].unsqueeze(1))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print('Epoch [%d/%d], Loss: %.4f' % (epoch+1, num_epochs, running_loss/len(train_data)))
+
+# Evaluate PyTorch model on validation set
+with torch.no_grad():
+    val_outputs = model(val_data[:, 0:4])
+    val_loss = criterion(val_outputs, val_data[:, 4].unsqueeze(1))
+    print('Validation Loss: %.4f' % val_loss.item())

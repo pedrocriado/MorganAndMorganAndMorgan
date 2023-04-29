f893bcc729ed74a7d4ac5a78185a4bb96b5fa034
pedrocriado <pedrocriadof2004@gmail.com>
2023-04-29 06:38:01 -0400

    Co-authored-by: Ian <astro-ion13@users.noreply.github.com> Co-authored-by: Josh Feldman <JoshFeldtheMan@users.noreply.github.com>

--- hello.py
+++ hello.py
@@ -1,3 +1,59 @@
-print("hello world")
-for x in range(2000):
-    print(x) #LOL we don't talk abt that
+import torch
+
+# Define a function to evaluate the cost of a schedule
+def cost(schedule):
+  # Compute the cost of the schedule based on the user's preferences and constraints
+  ...
+
+# Define a function to generate a perturbed schedule using the transition kernel
+def perturb(schedule):
+  # Select a random event from a random user's schedule
+  user = random.choice(schedule.keys())
+  event = random.choice(schedule[user])
+  
+  # Propose a new start time and duration for the event
+  new_start_time = random.uniform(event['earliest_start_time'], event['latest_start_time'])
+  new_duration = random.uniform(event['min_duration'], event['max_duration'])
+  
+  # Create a new schedule with the perturbed event
+  new_schedule = deepcopy(schedule)
+  new_schedule[user][event['index']]['start_time'] = new_start_time
+  new_schedule[user][event['index']]['duration'] = new_duration
+  
+  return new_schedule
+
+# Define the MCMC algorithm
+def mcmc(initial_schedule, iterations, temperature):
+  # Initialize the current schedule and its cost
+  current_schedule = initial_schedule
+  current_cost = cost(current_schedule)
+  
+  # Initialize the best schedule and its cost
+  best_schedule = current_schedule
+  best_cost = current_cost
+  
+  # Run the MCMC algorithm
+  for i in range(iterations):
+    # Generate a new schedule by perturbing the current schedule
+    new_schedule = perturb(current_schedule)
+    
+    # Evaluate the new schedule
+    new_cost = cost(new_schedule)
+    
+    # Calculate the acceptance probability
+    delta_cost = new_cost - current_cost
+    acceptance_prob = exp(-delta_cost / temperature)
+    
+    # Decide whether to accept or reject the new schedule
+    if delta_cost < 0 or random.random() < acceptance_prob:
+      current_schedule = new_schedule
+      current_cost = new_cost
+      
+      # Update the best schedule if necessary
+      if current_cost < best_cost:
+        best_schedule = current_schedule
+        best_cost = current_cost
+  
+  return best_schedule
+
+
